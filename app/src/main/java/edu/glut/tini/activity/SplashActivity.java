082452fe16e465617eb20678b4ed11574a01358c
Ardient <ardien@126.com>
2020-06-22 18:22:45 +0800

    init

---
+++ app/src/main/java/edu/glut/tini/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package edu.glut.tini.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
