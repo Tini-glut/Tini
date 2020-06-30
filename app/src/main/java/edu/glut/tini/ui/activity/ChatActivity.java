2f654230a5e3727deebb27c0629640db10179b28
lhdigo <lhdigo@qq.com>
2020-06-30 09:58:56 +0800

    修复发送空格会报错的bug

--- app/src/main/java/edu/glut/tini/ui/activity/ChatActivity.java
+++ app/src/main/java/edu/glut/tini/ui/activity/ChatActivity.java
@@ -2,15 +2,12 @@ package edu.glut.tini.ui.activity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.UiThread;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -32,21 +29,23 @@ public class ChatActivity extends BaseActivity implements ChatContract.View {
         return R.layout.activity_chat;
     }
 
-    private ImageView back;
+    private ImageView chat_back;
     private TextView chatTitle;
     private EditText textEdit;
     private Button btn_send;
     private RecyclerView recyclerView;
     private ChatPresenter chatPresenter = new ChatPresenter(this);
     private String userName;
-    private LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-    private EMMessageListener emMessageListener = new EMMessageListenerAdapter(){
+    private EMMessageListener emMessageListener = new EMMessageListenerAdapter() {
+
+        //收到消息
         @Override
         public void onMessageReceived(List<EMMessage> list) {
             super.onMessageReceived(list);
-            chatPresenter.addMessage(userName,list);
+            chatPresenter.addMessage(userName, list);
             runOnUiThread(() -> {
                 recyclerView.getAdapter().notifyDataSetChanged();
+                //滚动到底部
                 scrollToBottom();
             });
         }
@@ -56,47 +55,59 @@ public class ChatActivity extends BaseActivity implements ChatContract.View {
     public void init() {
         super.init();
         recyclerView = findViewById(R.id.chat_recyclerview);
-        linearLayoutManager.setReverseLayout(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        back = findViewById(R.id.chat_back);
+        chat_back = findViewById(R.id.chat_back);
         chatTitle = findViewById(R.id.chat_title);
         textEdit = findViewById(R.id.chat_edit);
         btn_send = findViewById(R.id.btn_send);
+
+        //聊天信息监听
         EMClient.getInstance().chatManager().addMessageListener(emMessageListener);
+
+        //获取聊天对象用户名
         userName = getIntent().getStringExtra("userName");
-        System.out.println("ChatActivity :  "+userName);
-        recyclerView.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
-            recyclerView.scrollToPosition(chatPresenter.getMessages().size()-1);
-        });
 
         //聊天界面左上角返回键监听事件
-        back.setOnClickListener(v -> {
+        chat_back.setOnClickListener(v -> {
             finish();
         });
+
+        //聊天界面header标题    聊天对象用户名
         chatTitle.setText(userName);
 
-
+        //初始化输入框  包括监听发送按钮
         initEditText(textEdit);
+        //初始化聊天界面 包括设置适配器
+        initRecyclerView();
+
+        //点击发送按钮的监听事件
         btn_send.setOnClickListener(v -> {
             hideSoftKeyboard();
-            chatPresenter.sendMessage(userName, textEdit.getText().toString().trim());
+            chatPresenter.sendMessage(userName, textEdit.getText().toString());
         });
 
-        initRecyclerView();
 
     }
 
 
-
+    /**
+     * 初始化 RecyclerView
+     */
     private void initRecyclerView() {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        recyclerView.setAdapter(new MessageListAdapter(this,chatPresenter.getMessages()));
+        //打开软键盘时 recyclerview滚动到底部
+        recyclerView.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+            recyclerView.scrollToPosition(chatPresenter.getMessages().size() - 1);
+        });
+        recyclerView.setAdapter(new MessageListAdapter(this, chatPresenter.getMessages()));
 
     }
 
-
+    /**
+     * 初始化输入框
+     *
+     * @param textEdit
+     */
     private void initEditText(EditText textEdit) {
         btn_send = findViewById(R.id.btn_send);
         textEdit.addTextChangedListener(new TextWatcher() {
@@ -133,7 +144,6 @@ public class ChatActivity extends BaseActivity implements ChatContract.View {
 
     @Override
     public void onSendMessageSuccess() {
-
         recyclerView.getAdapter().notifyDataSetChanged();
         Toast.makeText(this, "发送成功", Toast.LENGTH_SHORT).show();
         textEdit.getText().clear();
@@ -142,9 +152,11 @@ public class ChatActivity extends BaseActivity implements ChatContract.View {
 
     }
 
-
+    /**
+     * 聊天界面  滚动到屏幕底部
+     */
     private void scrollToBottom() {
-        recyclerView.scrollToPosition(chatPresenter.getMessages().size()-1);
+        recyclerView.scrollToPosition(chatPresenter.getMessages().size() - 1);
     }
 
     @Override
