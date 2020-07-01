f295698a16dff218037212be230db4be7e82830e
Ardient <ardien@126.com>
2020-07-01 22:34:47 +0800

    修复已知问题

--- app/src/main/java/edu/glut/tini/ui/activity/ChatActivity.java
+++ app/src/main/java/edu/glut/tini/ui/activity/ChatActivity.java
@@ -14,6 +14,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
 import com.hyphenate.chat.EMMessage;
 
 import java.util.List;
@@ -225,6 +226,10 @@ public class ChatActivity extends BaseActivity implements ChatContract.View {
     @Override
     protected void onPause() {
         super.onPause();
-        EMClient.getInstance().chatManager().getConversation(userName).markAllMessagesAsRead();
+
+        EMConversation conversation = EMClient.getInstance().chatManager().getConversation(userName);
+        if (conversation != null) {
+            conversation.markAllMessagesAsRead();
+        }
     }
 }
